1be157bd1f2a7b6a3621bbfc0b14d03bed3afe5f
Smith <ayden.smith@hpe.com>
2018-08-02 08:37:32 -0600

    File server upload capability with a home html webpage

--- cmd/fileserver/fileserver.go
+++ cmd/fileserver/fileserver.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"flag"
-	"github.com/gorilla/mux"
-	"net/http"
-	"time"
-	"log"
-
-	"io/ioutil"
-	"github.houston.softwaregrp.net/CSB/fileserver/pkg/fileserver"
 	"fmt"
+	"github.com/gorilla/mux"
+		//"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+	"mime/multipart"
+	"github.houston.softwaregrp.net/CSB/fileserver/pkg/fileserver"
+	"io"
 )
 
 type Init struct {
@@ -22,26 +25,39 @@ func main() {
 	flag.StringVar(&dir, "dir", "Public", "the directory to serve files from. Defaults to the current dir")
 	flag.StringVar(&port, "port", ":9993", "The Port the server is listening on")
 	flag.Parse()
-	i:=Init{dir}
-	p:=&i
+	i := Init{dir}
+	p := &i
 	fmt.Print(p)
-	fmt.Print(*p)
-	r :=mux.NewRouter()
+	r := mux.NewRouter()
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
 	r.HandleFunc("/", i.HandleRoot)
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/",http.FileServer(http.Dir(dir))))
+go	r.HandleFunc("/upload/", i.HandleUpload)
+	//r.HandleFunc("/static/",i.HandleStatic)
 	srv := &http.Server{
-		Handler: r,
-		Addr:    port,
+		Handler:      r,
+		Addr:         port,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
-
 	log.Fatal(srv.ListenAndServe())
 }
 
+func (i *Init) HandleRoot(w http.ResponseWriter, r *http.Request) {
 
+	path := fmt.Sprint("./Public/hello.html")
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Print(err, "\n 1 \n")
+	}
+	fileContents, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Print(err, "\n 2 \n")
+	}
+	w.Header().Set("Content-Type", "text/html")
+	w.Write(fileContents)
+	}
 
-func (i *Init)HandleRoot(w http.ResponseWriter,r *http.Request) {
+func (i *Init)HandleStatic(w http.ResponseWriter,r *http.Request) {
 	fmt.Print("dir",i.Dir)
 	bob := RipFunc(i.Dir)
 	rob:=fmt.Sprintf("%s%s%s","<ul>",bob,"</ul>")
@@ -49,6 +65,46 @@ func (i *Init)HandleRoot(w http.ResponseWriter,r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/html")
 	w.Write([]byte(rob))
+	fmt.Print(rob)
+}
+func (i *Init) HandleUpload(w http.ResponseWriter, r *http.Request){ //string,string,*multipart.FileHeader,string) {
+if r.Method=="POST" {
+	fmt.Print("\n here2 \n")
+	patho, _ := os.Getwd()
+	l, handler := PullUploader(r, w)
+	pather := string(patho + "/" + handler.Filename)
+	i.Move(pather, patho, handler, l)
+
+}else{
+	fmt.Fprint(w,"405 Method not allowed")
+}
+
+}
+func PullUploader(r *http.Request,w http.ResponseWriter)(string,*multipart.FileHeader) {
+
+	file, header, err := r.FormFile("uploadfile")
+	if err != nil {
+		fmt.Fprintln(w, err)
+
+	}
+	defer file.Close()
+
+	out, err := os.Create(header.Filename)
+	if err != nil {
+		fmt.Print(err)
+}
+
+	defer out.Close()
+
+	_, err = io.Copy(out, file)
+	if err != nil {
+		fmt.Fprintln(w, err)
+	}
+
+	l := r.FormValue("filepath")
+	fmt.Print("\n here5 \n")
+	fmt.Fprint(w,"200 File successfully uploaded")
+	return l,header
 }
 
 func RipFunc (root string) (fum string) {
@@ -66,4 +122,24 @@ func RipFunc (root string) (fum string) {
 	}
 
 	return
+}
+
+
+func (i *Init)Move(pather string ,patho string, handler *multipart.FileHeader,l string) {
+
+	if l == pather {
+		npat := string("./"+l+handler.Filename)
+		fmt.Print("\n npat2:", npat)
+		err := os.Rename(pather, npat)
+		if err != nil {
+			fmt.Print(err)
+		}
+	}else{
+		os.MkdirAll(l,0666)
+		npat := string("./"+l+handler.Filename)
+		err := os.Rename(pather, npat)
+		if err != nil {
+			fmt.Print(err)
+		}
+	}
 }
--- pkg/fileserver/fileserver.go
+++ pkg/fileserver/fileserver.go
@@ -2,12 +2,31 @@ package fileserver
 
 import (
 	"fmt"
+	"mime/multipart"
 	"os"
 )
 
 func FileFilter(f os.FileInfo) (fe string) {
-		if f.IsDir() == false {
-			fe= fmt.Sprint("<li>","<a href=","/static/",f.Name()," download target=","_blank",">",f.Name(),"</a>","</li>")
-		}
+
+	fe= fmt.Sprint("<li>","<a href=","/static/",f.Name(),">",f.Name(),"</a>","</li>")
 	return
+}
+
+func Move(pather string, patho string, handler *multipart.FileHeader, l string, Dir string) {
+	if l == "" {
+		npat := fmt.Sprint(patho + "/" + Dir + "/" + handler.Filename)
+		fmt.Print("\n npat1:", npat)
+		err := os.Rename(pather, npat)
+		if err != nil {
+			fmt.Print(err)
+		}
+	} else {
+		npat := fmt.Sprint(patho + l + handler.Filename)
+		fmt.Print("\n npat2:", npat)
+		err := os.Rename(pather, npat)
+		if err != nil {
+			fmt.Print(err)
+		}
+	}
+
 }
